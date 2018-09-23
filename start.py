59cddcb0f89982d8934aa6a6979d5b06d9351eae
Andrey <i@dru.su>
2018-09-23 20:28:37 +0700

    Add sqlalchemy and database support

--- start.py
+++ start.py
@@ -1,12 +1,32 @@
-from collections import namedtuple
-
 from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/py_sweater'
+db = SQLAlchemy(app)
 
-Message = namedtuple('Message', 'text tag')
-messages = []
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.text = text.strip()
+        self.tags = [
+            Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+
+db.create_all()
 
 
 @app.route('/', methods=['GET'])
@@ -16,7 +36,7 @@ def hello_world():
 
 @app.route('/main', methods=['GET'])
 def main():
-    return render_template('main.html', messages=messages)
+    return render_template('main.html', messages=Message.query.all())
 
 
 @app.route('/add_message', methods=['POST'])
@@ -24,6 +44,7 @@ def add_message():
     text = request.form['text']
     tag = request.form['tag']
 
-    messages.append(Message(text, tag))
+    db.session.add(Message(text, tag))
+    db.session.commit()
 
     return redirect(url_for('main'))
